d61fceb20af9abeeb20234f5cd7bc1663f29fad2
Owen Brooks <owen.h.brooks@gmail.com>
2022-05-02 22:40:47 +1000

    Add launching of ros1 bridge in run script
    In window 2

--- docker/tmux-start.sh
+++ docker/tmux-start.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 session="mcav"
-tmux new-session -A -s $session
+tmux new-session -d -s $session
+tmux send-keys 'source env/bin/activate'
+tmux send-keys Enter C-l
+tmux new-window
+tmux send-keys 'source /opt/ros/noetic/setup.bash && source /opt/ros/foxy/setup.bash && ros2 run ros1_bridge dynamic_bridge' C-l
+tmux split-window -h
+tmux send-keys 'source /opt/ros/noetic/setup.bash && roscore' C-l
+tmux select-window -t 1
+tmux attach-session -d -t $session
