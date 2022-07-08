595b6cad98422ca5edf5aacfe62b801b190dcde4
Byron-Gel <bgel0002@student.monash.edu>
2022-07-08 16:26:52 +1000

    working lane detection

--- md_lane_detection/md_lane_detection/sense_node.py
+++ md_lane_detection/md_lane_detection/sense_node.py
@@ -43,16 +43,17 @@ class Sense(Node):
                 slope = fit[0]
                 intercept = fit[1]
                 if slope < 0: # y is reversed in image
-                    left_fit.append((max(slope,-999), intercept))         
+                    left_fit.append((slope, intercept))         
                 else:
-                    right_fit.append((min(slope,999), intercept))
+                    right_fit.append((slope, intercept))   
         # add more weight to longer lines
-        left_fit_average  = np.average(left_fit, axis=0)
-        right_fit_average = np.average(right_fit, axis=0)
-        left_line  = self.make_points(image, left_fit_average)
-        right_line = self.make_points(image, right_fit_average)
-        averaged_lines = [left_line, right_line]
-        return averaged_lines
+        if len(left_fit) and len(right_fit):
+            left_fit_average  = np.average(left_fit, axis=0)
+            right_fit_average = np.average(right_fit, axis=0)
+            left_line  = self.make_points(image, left_fit_average)
+            right_line = self.make_points(image, right_fit_average)
+            averaged_lines = [left_line, right_line]
+            return averaged_lines
 
     def canny(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -65,13 +66,12 @@ class Sense(Node):
         line_image = np.zeros_like(img)
         if lines is not None:
             for line in lines:
-                print(line)
                 for x1, y1, x2, y2 in line:
                     #x1 = int(line[0][0])
                     #y1 = int(line[0][1])
                     #x2 = int(line[0][2])
                     #y2 = int(line[0][3])           
-                    cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),10)
+                    cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),100)
         return line_image
 
     def region_of_interest(self, canny):
@@ -80,9 +80,10 @@ class Sense(Node):
         mask = np.zeros_like(canny)
 
         triangle = np.array([[
-        (200, height),
-        (550, 250),
-        (1100, height),]], np.int32)
+        (0,height),
+        (0,height*0.55),
+        (width,height*0.55),
+        (width,height)]], np.int32)
 
         cv2.fillPoly(mask, triangle, 255)
         masked_image = cv2.bitwise_and(canny, mask)
@@ -95,7 +96,7 @@ class Sense(Node):
         '''
         try:
           bridge = CvBridge()
-          cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+          cv_image = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')       
           canny_image = self.canny(cv_image)
           cropped_canny = self.region_of_interest(canny_image)
           lines = cv2.HoughLinesP(cropped_canny, 2, np.pi/180, 100, np.array([]), minLineLength=40,maxLineGap=5)
