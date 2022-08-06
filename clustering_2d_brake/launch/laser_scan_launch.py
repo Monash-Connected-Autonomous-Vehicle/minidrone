f4527ff4af3708a3e33fd79b18d76886fb49ba9c
David-Helmerson <davidfhelmerson@gmail.com>
2022-08-06 21:23:40 +1000

    Updates

--- clustering_2d_brake/launch/laser_scan_launch.py
+++ clustering_2d_brake/launch/laser_scan_launch.py
@@ -1,16 +1,23 @@
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
 
 def generate_launch_description():
+    lidar_share = get_package_share_directory('rplidar_ros2')
+
     return LaunchDescription([
-        Node(
-            package='rplidar_ros2',
-            executable='rplidar_scan_publisher',
-        ),
+        IncludeLaunchDescription([
+            PythonLaunchDescriptionSource([lidar_share, '/launch/rplidar_launch.py'])
+        ]),
+
         Node(
             package='clustering_2d_brake',
             executable='scan_to_cloud',
         ),
+
         Node(
             package='clustering_2d_brake',
             executable='cluster_2d',
