be15e7b9ef2fe836576d3428d54abc2e8ef53718
saadtaslaq <saadtaslaq@gmail.com>
2022-09-21 14:11:13 +1000

    Addition of config and launch file for EKF

---
+++ robot_localisation_pkg/launch/robot_localisation_launch.py
@@ -0,0 +1,89 @@
+# Copyright (c) 2021 Juan Miguel Jimeno
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    use_sim_time = True
+
+    ekf_config_path = PathJoinSubstitution(
+        ["config", "ekf.yaml"]
+    )
+
+    # description_launch_path = PathJoinSubstitution(
+    #     [FindPackageShare('linorobot2_description'), 'launch', 'minidrone_description.launch.py']
+    # )
+
+    return LaunchDescription([
+        # DeclareLaunchArgument(
+        #     name='world', 
+        #     default_value=world_path,
+        #     description='Gazebo world'
+        # ),
+
+        # ExecuteProcess(
+        #     cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',  '-s', 'libgazebo_ros_init.so', LaunchConfiguration('world')],
+        #     output='screen'
+        # ),
+
+        # Node(
+        #     package='gazebo_ros',
+        #     executable='spawn_entity.py',
+        #     name='urdf_spawner',
+        #     output='screen',
+        #     arguments=["-topic", "robot_description", "-entity", "linorobot2"]
+        # ),
+
+        # Node(
+        #     package='linorobot2_gazebo',
+        #     executable='command_timeout.py',
+        #     name='command_timeout'
+        # ),
+
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[
+                {'use_sim_time': use_sim_time}, 
+                ekf_config_path
+            ],
+            remappings=[("odometry/filtered", "odom")]
+        ),
+
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource(description_launch_path),
+        #     launch_arguments={
+        #         'use_sim_time': str(use_sim_time),
+        #         'publish_joints': 'false',
+        #     }.items()
+        # ),
+
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource(joy_launch_path),
+        # )
+    ])
+
+#sources: 
+#https://navigation.ros.org/setup_guides/index.html#
+#https://answers.ros.org/question/374976/ros2-launch-gazebolaunchpy-from-my-own-launch-file/
+#https://github.com/ros2/rclcpp/issues/940
